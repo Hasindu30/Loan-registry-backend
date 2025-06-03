88acb2bbb29facff5e2d0d88410e7190a4d82677
Hasindu Chandeepa <chandeepahasindu@gmail.com>
2025-06-04 00:53:58 +0530

    update issue

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ import pdfRoutes from './routes/pdfRoutes.js';
 
 connectToDatabase()
  const app  = express()
- app.use(cors())
+ app.use(cors({
+    origin: 'https://loan-registry-frontend.vercel.app',
+  credentials: true
+ }))
  app.use(express.json())
  app.use('/api/auth',authRouter)
 
