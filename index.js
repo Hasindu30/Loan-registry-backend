f41cab40e86544a79c6e18fd516cd614a16e4928
Hasindu Chandeepa <chandeepahasindu@gmail.com>
2025-06-07 15:19:59 +0530

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
-dotenv.config({ path: path.resolve(__dirname, '.env') });
+dotenv.config();
 
 
 import express from 'express';
